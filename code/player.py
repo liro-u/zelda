26e72073e889a2b508bc53d57f978d3e02e00164
liro-u <86537787+liro-u@users.noreply.github.com>
2022-07-18 23:10:28 +0200

    Initial commit

---
+++ code/player.py
@@ -0,0 +1,201 @@
+from turtle import right
+from numpy import character
+import pygame
+from settings import *
+from support import import_folder
+
+class Player(pygame.sprite.Sprite):
+  def __init__(self,pos,groups,obstacle_sprites,create_attack,destroy_attack,create_magic,destroy_magic):
+    super().__init__(groups)
+    self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
+    self.rect = self.image.get_rect(topleft = pos)
+    self.hitbox = self.rect.inflate(0,-26)
+
+    # graphics setup
+    self.import_player_assets()
+    self.status = 'down'
+    self.frame_index = 0
+    self.animation_speed = 0.15
+
+    # movement
+    self.direction = pygame.math.Vector2()
+    self.attacking = False
+    self.attack_cooldown = 400
+    self.attack_time = None
+
+    self.obstacle_sprites = obstacle_sprites
+    
+    # common to weapon and magic
+    self.switch_duration_cooldown = 200
+
+    # weapon
+    self.create_attack = create_attack
+    self.destroy_attack = destroy_attack
+    self.weapon_index = 0
+    self.weapon = list(weapon_data.keys())[self.weapon_index]
+    self.can_switch_weapon = True
+    self.weapon_switch_time = None
+
+    # magic
+    self.create_magic = create_magic
+    self.destroy_magic = destroy_magic
+    self.magic_index = 0
+    self.magic = list(magic_data.keys())[self.magic_index]
+    self.can_switch_magic = True
+    self.magic_switch_time = None
+
+    # stats
+    self.stats = {'health':100,'energy':60,'attack':10,'magic':4,'speed':5}
+    self.health = self.stats['health']
+    self.energy = self.stats['energy']
+    self.exp = 123
+    self.speed = self.stats['speed']
+
+  def import_player_assets(self):
+    character_path = '../graphics/player/'
+    self.animations = {
+      'up':[],"down":[],'left':[],'right':[],
+      'up_idle':[],"down_idle":[],'left_idle':[],'right_idle':[],
+      'up_attack':[],"down_attack":[],'left_attack':[],'right_attack':[],
+    }
+
+    for animation in self.animations.keys():
+      full_path = character_path + animation
+      self.animations[animation] = import_folder(full_path)
+
+  def is_action_pressed(self,action_name,keys):
+    for key_name in action_name.value:
+      if keys[key_name]:
+        return True
+    return False
+
+  def input(self):
+    if not self.attacking:
+      keys = pygame.key.get_pressed()
+
+      # movement input
+      if self.is_action_pressed(Action.MOVE_UP,keys):
+        self.direction.y = -1
+        self.status = 'up'
+      elif self.is_action_pressed(Action.MOVE_DOWN,keys):
+        self.direction.y = 1
+        self.status = 'down'
+      else:
+        self.direction.y = 0
+
+      if self.is_action_pressed(Action.MOVE_LEFT,keys):
+        self.direction.x = -1
+        self.status = 'left'
+      elif self.is_action_pressed(Action.MOVE_RIGHT,keys):
+        self.direction.x = 1
+        self.status = 'right'
+      else:
+        self.direction.x = 0
+
+      # attack input
+      if self.is_action_pressed(Action.ATTACK,keys):
+        self.attacking = True
+        self.attack_time = pygame.time.get_ticks()
+        self.create_attack()
+
+      if self.is_action_pressed(Action.SWITCH_WEAPON,keys) and self.can_switch_weapon:
+        self.can_switch_weapon = False
+        self.weapon_switch_time = pygame.time.get_ticks()
+        if self.weapon_index < len(list(weapon_data.keys())) - 1:
+          self.weapon_index += 1
+        else:
+          self.weapon_index = 0
+        self.weapon = list(weapon_data.keys())[self.weapon_index]
+      
+      # magic input
+      if self.is_action_pressed(Action.MAGIC,keys):
+        self.attacking = True
+        self.attack_time = pygame.time.get_ticks()
+        style = self.magic
+        strength = list(magic_data.values())[self.magic_index]['strength'] + self.stats['magic']
+        cost = list(magic_data.values())[self.magic_index]['cost']
+        self.create_magic(style,strength,cost)
+      
+      if self.is_action_pressed(Action.SWITCH_MAGIC,keys) and self.can_switch_magic:
+        self.can_switch_magic = False
+        self.magic_switch_time = pygame.time.get_ticks()
+        if self.magic_index < len(list(magic_data.keys())) - 1:
+          self.magic_index += 1
+        else:
+          self.magic_index = 0
+        self.magic = list(magic_data.keys())[self.magic_index]
+
+  def get_status(self):
+
+    # idle status
+    if self.direction.x == 0 and self.direction.y == 0:
+      if not 'idle' in self.status and not 'attack' in self.status:
+        self.status += '_idle'
+    
+    # attack status
+    if self.attacking:
+      self.direction.x = 0
+      self.direction.y = 0
+      if not 'attack' in self.status:
+        if 'idle' in self.status:
+          self.status = self.status.replace('_idle','_attack')
+        else:
+          self.status = self.status + '_attack'
+    else:
+      if 'attack' in self.status:
+        self.status = self.status.replace('_attack','')
+    
+  def move(self,speed):
+    if self.direction.magnitude() != 0:
+      self.direction = self.direction.normalize()
+
+    self.hitbox.x += self.direction.x * speed
+    self.collision('horizontal')
+    self.hitbox.y += self.direction.y * speed
+    self.collision('vertical')
+    self.rect.center = self.hitbox.center
+
+  def collision(self,direction):
+    if direction == 'horizontal':
+      for sprite in self.obstacle_sprites:
+        if sprite.hitbox.colliderect(self.hitbox):
+          if self.direction.x > 0: # moving right
+            self.hitbox.right = sprite.hitbox.left
+          if self.direction.x < 0: # moving left
+            self.hitbox.left = sprite.hitbox.right
+    
+    if direction == 'vertical':
+      for sprite in self.obstacle_sprites:
+        if sprite.hitbox.colliderect(self.hitbox):
+          if self.direction.y > 0: # moving down
+            self.hitbox.bottom = sprite.hitbox.top
+          if self.direction.y < 0: # moving top
+            self.hitbox.top = sprite.hitbox.bottom
+
+  def cooldowns(self):
+    current_time = pygame.time.get_ticks()
+    if self.attacking:
+      if current_time - self.attack_time >= self.attack_cooldown:
+        self.attacking = False
+        self.destroy_attack()
+    if not self.can_switch_weapon:
+      if current_time - self.weapon_switch_time >= self.switch_duration_cooldown:
+        self.can_switch_weapon = True
+    if not self.can_switch_magic:
+      if current_time - self.magic_switch_time >= self.switch_duration_cooldown:
+        self.can_switch_magic = True
+
+  def animate(self):
+    animation = self.animations[self.status]
+    self.frame_index += self.animation_speed
+    if self.frame_index >= len(animation):
+      self.frame_index = 0
+    self.image = animation[int(self.frame_index)]
+    self.rect = self.image.get_rect(center = self.hitbox.center)
+
+  def update(self):
+    self.input()
+    self.cooldowns()
+    self.get_status()
+    self.animate()
+    self.move(self.speed)
---
+++ code/settings.py
@@ -0,0 +1,57 @@
+import pygame
+from enum import Enum
+
+# game setup
+WIDTH = 1280
+HEIGTH = 720
+FPS = 60
+TILESIZE = 64
+
+class Action(Enum):
+  # movement
+  MOVE_DOWN = [pygame.K_DOWN,pygame.K_s]
+  MOVE_UP = [pygame.K_UP,pygame.K_z]
+  MOVE_LEFT = [pygame.K_LEFT,pygame.K_q]
+  MOVE_RIGHT = [pygame.K_RIGHT,pygame.K_d]
+
+  # attack
+  ATTACK = [pygame.K_SPACE]
+  SWITCH_WEAPON = [pygame.K_a]
+
+  # magic
+  MAGIC = [pygame.K_LCTRL]
+  SWITCH_MAGIC = [pygame.K_e]
+
+# UI
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT = '../graphics/font/joystix.ttf'
+UI_FONT_SIZE = 18
+
+# general colors
+WATER_COLOR = '#71ddee'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
+
+# ui colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
+
+# weapons
+weapon_data = {
+  'sword': {'cooldown':100,'damage':15,'graphic':'../graphics/weapons/sword/full.png'},
+  'lance': {'cooldown':400,'damage':30,'graphic':'../graphics/weapons/lance/full.png'},
+  'axe': {'cooldown':300,'damage':20,'graphic':'../graphics/weapons/axe/full.png'},
+  'rapier': {'cooldown':50,'damage':8,'graphic':'../graphics/weapons/rapier/full.png'},
+  'sai': {'cooldown':80,'damage':10,'graphic':'../graphics/weapons/sai/full.png'},
+}
+
+# magic
+magic_data = {
+  'flame': {'strength':5,'cost':20,'graphic':'../graphics/particles/flame/fire.png'},
+  'heal': {'strength':20,'cost':10,'graphic':'../graphics/particles/heal/heal.png'},
+}
